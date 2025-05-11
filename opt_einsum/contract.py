a58c22f775b0e443e844862899593fa786d6f6b6
DanisNone <gubajdullindanis@gmail.com>
2025-05-12 01:01:43 +0500

    Bug fix: when calling contract with optimize=False, _einsum did not receive any backend and used numpy by default. Now the logic has been corrected.

--- opt_einsum/contract.py
+++ opt_einsum/contract.py
@@ -581,7 +581,8 @@ def contract(
 
     # If no optimization, run pure einsum
     if optimize is False:
-        return _einsum(*operands_list, out=out, **kwargs)
+        backend = parse_backend(operands, backend)
+        return _einsum(*operands_list, backend=backend, out=out, **kwargs)
 
     # Grab non-einsum kwargs
     gen_expression = kwargs.pop("_gen_expression", False)
